371f0bf9ca716e3009c87c73be7b56ef52e9cd61
Mihai <sbarceamihai222@gmail.com>
2026-02-16 21:53:55 +0200

    Update entrypoint.sh

--- docker-images/minecraft/paper/entrypoint.sh
+++ docker-images/minecraft/paper/entrypoint.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 set -e
 
-# Default version if not specified
-MINECRAFT_VERSION=${MINECRAFT_VERSION:-"1.20.1"}
+# Internal variables
+export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
+# Move to the container directory
+cd /home/container || exit 1
+
+# Download Paper if it doesn't exist
 if [ ! -f paper.jar ]; then
-    echo "Downloading Minecraft Paper $MINECRAFT_VERSION..."
-    # Note: In a real scenario, we'd fetch the latest build from the PaperMC API properly.
-    # This URL is an example for 1.20.1 build 196.
-    curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/196/downloads/paper-1.20.1-196.jar
+    echo "Downloading Minecraft Paper (latest build)..."
+    # This is a simplified download, in production you'd use the Paper API to find the latest build for a version
+    curl -sSL "https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/381/downloads/paper-1.20.4-381.jar" -o paper.jar
 fi
 
+# Ensure EULA is accepted
 if [ ! -f eula.txt ]; then
     echo "eula=true" > eula.txt
 fi
 
-exec "$@"
+# Print info
+echo "Server starting on $INTERNAL_IP"
+java -version
+
+# Execute the startup command provided by the panel
+eval "${STARTUP}"
